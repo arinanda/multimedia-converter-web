72e17ef18f112d9400a7c1279246dad679b688ae
Muhammad Adib Arinanda <arinanda.adib@gmail.com>
2018-10-03 07:54:41 +0700

    adding cloudconvert dependencies

--- config/app.php
+++ config/app.php
@@ -146,6 +146,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        RobbieP\CloudConvertLaravel\CloudConvertLaravelServiceProvider::class,
 
         /*
          * Package Service Providers...
